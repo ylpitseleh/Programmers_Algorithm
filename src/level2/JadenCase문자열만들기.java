209d26c357537d45070cd86afc4a1617a306a9e0
ylpitseleh <lyla02217@gmail.com>
2020-08-11 14:51:39 +0900

    Add JadenCase문자열만들기

---
+++ src/level2/JadenCase문자열만들기.java
@@ -0,0 +1,30 @@
+package level2;
+
+public class JadenCase문자열만들기 {
+
+	public static void main(String[] args) {
+		String s = "3people unFollowed me";
+		//String s = "for the last week";
+		
+		String answer = "";
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(Character.toString(s.charAt(0)).toUpperCase());
+		for (int i=1; i<s.length(); i++) {
+			String tmp = Character.toString(s.charAt(i));
+			if (s.charAt(i-1)==' ') {
+				tmp = tmp.toUpperCase();
+			}
+			else {
+				tmp = tmp.toLowerCase();
+			}
+			//System.out.print(tmp);
+			//System.out.println();
+			sb.append(tmp);
+			
+		}
+		
+		System.out.println(sb);
+		
+	}
+}
