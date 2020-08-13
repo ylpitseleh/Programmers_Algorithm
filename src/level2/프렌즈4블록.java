6e7f2e0db93ff863b9c459054a111bfabb0ddc9a
ylpitseleh <lyla02217@gmail.com>
2020-08-13 16:40:45 +0900

    Add 프렌즈4블록

---
+++ src/level2/프렌즈4블록.java
@@ -0,0 +1,32 @@
+package level2;
+
+import java.util.ArrayList;
+public class 프렌즈4블록 {
+	
+	public static void main(String[] args) {
+		String board[] = {  "CCBDE", 
+							"AAADE", 
+							"AAABF", 
+							"CCBBF" };
+		int m = 4; // h
+		int n = 5; // w
+		/*
+		 * String board[] = {"TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF","TMMTTJ"}; 
+		 * int m = 6; int n = 6;
+		 */
+
+		int answer = 0;
+
+		
+		/*
+		boolean breakFlag = false;
+		while (true) {
+			if (breakFlag)
+				break;
+
+		}
+		*/
+		
+	}
+
+}
